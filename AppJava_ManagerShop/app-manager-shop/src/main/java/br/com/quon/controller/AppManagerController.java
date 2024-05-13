56bbe16ccaaa5142584267b2316163ab9f5c7dd0
Matheus1221 <58350861+Matheus1221@users.noreply.github.com>
2024-05-13 14:20:41 -0300

    post and delete

--- AppJava_ManagerShop/app-manager-shop/src/main/java/br/com/quon/controller/AppManagerController.java
+++ AppJava_ManagerShop/app-manager-shop/src/main/java/br/com/quon/controller/AppManagerController.java
@@ -3,8 +3,11 @@ package br.com.quon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,14 @@ public class AppManagerController {
 	return service.findById(id);
 	}
 	
+	@PostMapping
+	public Client create(@RequestBody Client client) {
+		return service.update(client);
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public responseEntity<? delete(id)
 	
 	
 
